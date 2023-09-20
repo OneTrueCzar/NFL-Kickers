79c475591d8ecd4ae515584705f1dc51e7de5e27
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:39:08 -0400

    adding kicker summaries

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -3,10 +3,12 @@ library(nflreadr)
 library(nflfastR)
 
 kickers <- load_pbp(seasons = TRUE) %>% 
-  select(play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
+  select(game_id, posteam, play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
   filter(play_type == "field_goal",
          score_differential_post > 0 & score_differential_post <= 3,
          half_seconds_remaining <= 300,
          kicker_player_name == "J.Tucker" |
          kicker_player_name == "C.Boswell" |
          kicker_player_name == "E.McPherson")
+
+kicker_summary <- calculate_player_stats_kicking(kickers, weekly = TRUE)
