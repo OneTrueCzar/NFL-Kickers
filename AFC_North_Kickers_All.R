64d1b93e88be1fa3f2d8b5597e45a82ac8bd040a
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 13:19:45 -0400

    trying to get Overtime to calculate for the entire quarter

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -6,7 +6,8 @@ kickers <- load_pbp(seasons = TRUE) %>%
   select(season, week, season_type, play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, end_clock_time, desc, kicker_player_name) %>% 
   filter(play_type == "field_goal",
          score_differential_post >= 0 & score_differential_post <= 3,
-         half_seconds_remaining <= 300,
+         half_seconds_remaining <= 300 & game_half != "Overtime",
+         half_seconds_remaining > 0 & game_half == "Overtime",
          kicker_player_name == "J.Tucker" |
          kicker_player_name == "C.Boswell" |
          kicker_player_name == "E.McPherson")
