7e44f1e40a0f5e73ca6052fb1c982cf4669ba5c6
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 13:47:58 -0400

    reverting back to temporary commit and making some edits

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -2,16 +2,15 @@ library(tidyverse)
 library(nflreadr)
 library(nflfastR)
 
+pbp <- load_pbp(seasons = TRUE)
+
+kicker_summary <- calculate_player_stats_kicking(pbp, weekly = FALSE)
+
 kickers <- load_pbp(seasons = TRUE) %>% 
+  select(season, week, season_type, play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, end_clock_time, desc, kicker_player_name) %>% 
   filter(play_type == "field_goal",
-         score_differential_post > 0 & score_differential_post <= 3,
+         score_differential_post >= 0 & score_differential_post <= 3,
          half_seconds_remaining <= 300,
          kicker_player_name == "J.Tucker" |
-         kicker_player_name == "C.Boswell" |
-         kicker_player_name == "E.McPherson")
-
-kicker_summary <- calculate_player_stats_kicking(kickers, weekly = FALSE)
-
-kickers_detailed <- kickers %>% 
-  select(season, week, season_type, defteam, game_stadium, play_type, field_goal_result, score_differential_post, 
-         kick_distance, half_seconds_remaining, game_half, time, end_clock_time, kicker_player_name)
+           kicker_player_name == "C.Boswell" |
+           kicker_player_name == "E.McPherson")
