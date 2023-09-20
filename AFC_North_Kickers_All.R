6b66b43dfcaba8e294e0567303fcc8b63752b015
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:13:12 -0400

    testing justin tucker only

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -5,6 +5,4 @@ kickers <- load_pbp(seasons = TRUE) %>%
   select(play_type, field_goal_result, kick_distance, half_seconds_remaining, kicker_player_name) %>% 
   filter(play_type == "field_goal",
          half_seconds_remaining < 3000,
-         kicker_player_name == "Justin Tucker" |
-           kicker_player_name == "Chris Boswell" |
-           kicker_player_name == "Evan McPherson")
+         kicker_player_name == "Justin Tucker")
