ac318510d401e515e39cdfc09df373f740bb9763
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:12:54 -0400

    beginning test

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -1,2 +1,10 @@
 library(tidyverse)
 library(nflreadr)
+
+kickers <- load_pbp(seasons = TRUE) %>% 
+  select(play_type, field_goal_result, kick_distance, half_seconds_remaining, kicker_player_name) %>% 
+  filter(play_type == "field_goal",
+         half_seconds_remaining < 3000,
+         kicker_player_name == "Justin Tucker" |
+           kicker_player_name == "Chris Boswell" |
+           kicker_player_name == "Evan McPherson")
