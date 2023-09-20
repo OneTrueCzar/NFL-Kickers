50d9b694412794d5491cb606546e643cf82b5e60
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:32:10 -0400

    adding pre_kick score differential to verify that i can use post_kick score differential to indicate lead changes

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -3,7 +3,7 @@ library(nflreadr)
 library(nflfastR)
 
 kickers <- load_pbp(seasons = TRUE) %>% 
-  select(play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
+  select(play_type, field_goal_result, score_differential, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
   filter(play_type == "field_goal",
          half_seconds_remaining < 300,
          kicker_player_name == "J.Tucker" |
