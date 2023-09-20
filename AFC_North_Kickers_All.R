6b6e366b297f69b4bc999a7ca162c194bddf5374
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:15:45 -0400

    adjusted half_seconds_remaing from 3000 to 300 bc i cant math

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -4,4 +4,4 @@ library(nflreadr)
 kickers <- load_pbp(seasons = TRUE) %>% 
   select(play_type, field_goal_result, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
   filter(play_type == "field_goal",
-         half_seconds_remaining < 3000)
+         half_seconds_remaining < 300)
