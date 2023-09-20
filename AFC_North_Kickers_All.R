b9bfcd1fd3e3f288faae01dfe1a8f5a3e32a844e
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:33:23 -0400

    filtering for score diff post play of 3 or less to indicate lead changes

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -3,9 +3,10 @@ library(nflreadr)
 library(nflfastR)
 
 kickers <- load_pbp(seasons = TRUE) %>% 
-  select(play_type, field_goal_result, score_differential, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
+  select(play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
   filter(play_type == "field_goal",
-         half_seconds_remaining < 300,
+         score_differential_post <= 3,
+         half_seconds_remaining <= 300,
          kicker_player_name == "J.Tucker" |
          kicker_player_name == "C.Boswell" |
          kicker_player_name == "E.McPherson")
