efb319d5dd7f5535740cc7df6bb48946b557542b
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:20:00 -0400

    corrected references to kickers

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -3,4 +3,8 @@ library(nflreadr)
 
 kickers <- load_pbp(seasons = TRUE) %>% 
   select(play_type, field_goal_result, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
-  filter(play_type == "field_goal")
+  filter(play_type == "field_goal",
+         half_seconds_remaining < 300,
+         kicker_player_name == "J.Tucker" |
+         kicker_player_name == "C.Boswell" |
+         kicker_player_name == "E.McPherson")
