6d839bddf0c83f76cd48d7de0f7685492ddc0197
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 15:28:11 -0400

    okay i think i finally fucking got it correct now

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -2,11 +2,14 @@ library(tidyverse)
 library(nflreadr)
 library(nflfastR)
 
-pbp <- load_pbp(seasons = c("2018":"2023")) %>% 
+pbp <- load_pbp(seasons = c("2018":"2023")) %>%
   filter(play_type == "field_goal",
+         score_differential <= 0,
+         score_differential_post >= -3 & score_differential_post <= 3,
+         half_seconds_remaining <= 300 | game_half == "Overtime",
          kicker_player_name == "J.Tucker" |
-         kicker_player_name == "C.Boswell" |
-         kicker_player_name == "E.McPherson")
+           kicker_player_name == "C.Boswell" |
+           kicker_player_name == "E.McPherson")
 
 kicker_summary <- calculate_player_stats_kicking(pbp, weekly = FALSE) %>% 
   select(player_display_name, games, fg_made:fg_made_0_19, fg_missed_0_19, fg_made_20_29, fg_missed_20_29,
@@ -16,5 +19,5 @@ kicker_summary <- calculate_player_stats_kicking(pbp, weekly = FALSE) %>%
 
 kickers <- pbp %>% 
   select(season, week, season_type, defteam, game_stadium, play_type, field_goal_result, score_differential,
-         score_differential_post, kick_distance, half_seconds_remaining, game_half, time, end_clock_time,
+         score_differential_post, kick_distance, half_seconds_remaining, game_half, qtr, time, end_clock_time,
          kicker_player_name)
