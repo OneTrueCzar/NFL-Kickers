665faf7a122366671b0c40825f6800ab9c817c92
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:54:41 -0400

    adding game-tying FGs

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -5,7 +5,7 @@ library(nflfastR)
 kickers <- load_pbp(seasons = TRUE) %>% 
   select(play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
   filter(play_type == "field_goal",
-         score_differential_post > 0 & score_differential_post <= 3,
+         score_differential_post => 0 & score_differential_post <= 3,
          half_seconds_remaining <= 300,
          kicker_player_name == "J.Tucker" |
            kicker_player_name == "C.Boswell" |
