13b9203bcaad969e77903e4b9c35855bb7cf2943
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 13:09:41 -0400

    replace quarter_end with desc

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -3,7 +3,7 @@ library(nflreadr)
 library(nflfastR)
 
 kickers <- load_pbp(seasons = TRUE) %>% 
-  select(season, week, season_type, play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, end_clock_time, quarter_end, kicker_player_name) %>% 
+  select(season, week, season_type, play_type, field_goal_result, score_differential_post, kick_distance, half_seconds_remaining, game_half, time, end_clock_time, desc, kicker_player_name) %>% 
   filter(play_type == "field_goal",
          score_differential_post >= 0 & score_differential_post <= 3,
          half_seconds_remaining <= 300,
