c3dca21f70ff59d396e6232cd8ad385d4989eaf0
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 12:14:50 -0400

    removing justin tucker, adding game_half and time to df

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(nflreadr)
 
 kickers <- load_pbp(seasons = TRUE) %>% 
-  select(play_type, field_goal_result, kick_distance, half_seconds_remaining, kicker_player_name) %>% 
+  select(play_type, field_goal_result, kick_distance, half_seconds_remaining, game_half, time, kicker_player_name) %>% 
   filter(play_type == "field_goal",
-         half_seconds_remaining < 3000,
-         kicker_player_name == "Justin Tucker")
+         half_seconds_remaining < 3000)
