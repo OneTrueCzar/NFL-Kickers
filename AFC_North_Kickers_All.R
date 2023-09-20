fa50955d3440607a4e95299b7e2621976983c906
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 14:44:05 -0400

    seeing all-time GW

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -2,11 +2,8 @@ library(tidyverse)
 library(nflreadr)
 library(nflfastR)
 
-pbp <- load_pbp(seasons = c("2018":"2022")) %>% 
+pbp <- load_pbp(TRUE) %>% 
   filter(play_type == "field_goal",
-         score_differential <= 0,
-         score_differential_post >= 0 & score_differential_post <= 3,
-         half_seconds_remaining <= 300,
          kicker_player_name == "J.Tucker" |
          kicker_player_name == "C.Boswell" |
          kicker_player_name == "E.McPherson")
