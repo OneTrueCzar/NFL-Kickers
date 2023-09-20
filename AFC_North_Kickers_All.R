987e27498762253b08a653e5eeaa1e2d171deb03
Paul Kearney <pmkearney92@gmail.com>
2023-09-20 14:19:56 -0400

    made a bunch of changes that I didn't fully track, but the important one is having a score differential of under or equal to 0 to account for a possible miss of a non-lead change opportunity

--- AFC_North_Kickers_All.R
+++ AFC_North_Kickers_All.R
@@ -4,6 +4,7 @@ library(nflfastR)
 
 pbp <- load_pbp(seasons = c("2018":"2022")) %>% 
   filter(play_type == "field_goal",
+         score_differential <= 0,
          score_differential_post >= 0 & score_differential_post <= 3,
          half_seconds_remaining <= 300,
          kicker_player_name == "J.Tucker" |
@@ -11,8 +12,12 @@ pbp <- load_pbp(seasons = c("2018":"2022")) %>%
          kicker_player_name == "E.McPherson")
 
 kicker_summary <- calculate_player_stats_kicking(pbp, weekly = FALSE) %>% 
-  select(player_display_name, games, fg_made:fg_missed_60_, gwfg_att, gwfg_made:gwfg_blocked)
+  select(player_display_name, games, fg_made:fg_made_0_19, fg_missed_0_19, fg_made_20_29, fg_missed_20_29,
+         fg_made_30_39, fg_missed_30_39, fg_made_40_49, fg_missed_40_49, fg_made_50_59, fg_missed_50_59,
+         fg_made_60_, fg_missed_60_, gwfg_made, gwfg_att, gwfg_missed, gwfg_blocked) %>% 
+  mutate(gwfg_pct = round((`gwfg_made` / `gwfg_att`), digits = 3))
 
 kickers <- pbp %>% 
-  select(season, week, season_type, defteam, game_stadium, play_type, field_goal_result, score_differential_post,
-         kick_distance, half_seconds_remaining, game_half, time, end_clock_time, kicker_player_name)
+  select(season, week, season_type, defteam, game_stadium, play_type, field_goal_result, score_differential,
+         score_differential_post, kick_distance, half_seconds_remaining, game_half, time, end_clock_time,
+         kicker_player_name)
